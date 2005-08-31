13aeae9cefc84d177b9d2beb930a85081983c649
richieigenmann <>
2005-08-31 22:50:19 +0000

    Added proper escaping for special characters in HTML

--- src/java/jpo/HtmlDistillerThread.java
+++ src/java/jpo/HtmlDistillerThread.java
@@ -371,7 +371,7 @@ public class HtmlDistillerThread extends Thread {
 		out.write("<TR><TD colspan=" + Integer.toString(picsPerRow) + ">");
 		out.newLine();
 		
-		out.write("<H2>" + ((GroupInfo) groupNode.getUserObject()).getGroupName() + "</H2>");
+		out.write("<H2>" + stringToHTMLString ( ((GroupInfo) groupNode.getUserObject()).getGroupName() ) + "</H2>");
 		out.newLine();
 		
 		//link to parent
@@ -959,7 +959,7 @@ public class HtmlDistillerThread extends Thread {
 						+ Integer.toString(thumbnailWidth) 
 						+ ">");
 		
-					out.write(descriptions[i]);
+					out.write( stringToHTMLString( descriptions[i] ) );
 			
 					out.write("</TD>");
 					out.newLine();
@@ -1057,6 +1057,64 @@ public class HtmlDistillerThread extends Thread {
 			progressLabel.setText( Settings.jpoResources.getString("htmlDistillerInterrupt") );
 			interrupt = true;
 		}
+	}
+	
+
+	/**
+	 *  This method converts the special characters to codes that HTML can deal with.
+	 *  Taken from http://www.rgagnon.com/javadetails/java-0306.html
+	 */
+	public static String stringToHTMLString(String string) {
+		StringBuffer sb = new StringBuffer(string.length());
+		// true if last char was blank
+		boolean lastWasBlankChar = false;
+		int len = string.length();
+		char c;
+
+		for (int i = 0; i < len; i++) {
+			c = string.charAt(i);
+			if (c == ' ') {
+				// blank gets extra work,
+				// this solves the problem you get if you replace all
+				// blanks with &nbsp;, if you do that you loss 
+				// word breaking
+				if (lastWasBlankChar) {
+					lastWasBlankChar = false;
+					sb.append("&nbsp;");
+				} else {
+					lastWasBlankChar = true;
+					sb.append(' ');
+				}
+			} else {
+				lastWasBlankChar = false;
+				//
+				// HTML Special Chars
+				if (c == '"')
+					sb.append("&quot;");
+				else if (c == '&')
+					sb.append("&amp;");
+				else if (c == '<')
+					sb.append("&lt;");
+				else if (c == '>')
+					sb.append("&gt;");
+				else if (c == '\n')
+					// Handle Newline
+					sb.append("&lt;br/&gt;");
+				else {
+					int ci = 0xffff & c;
+					if (ci < 160 )
+						// nothing special only 7 Bit
+						sb.append(c);
+					else {
+						// Not 7 Bit use the unicode system
+						sb.append("&#");
+						sb.append(new Integer(ci).toString());
+						sb.append(';');
+					}
+				}
+			}
+		}
+    		return sb.toString();
 	}
 	
 		
