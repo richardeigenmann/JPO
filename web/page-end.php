f526d78f020585c892b7b8ffd44faf9ae0f5e2a4
Richard Eigenmann <richard.eigenmann@gmail.com>
2015-12-04 23:43:47 +0100

    Add Google Analytics

--- web/page-end.php
+++ web/page-end.php
@@ -1,7 +1,5 @@
-<!--        </td>
-    </tr>
-</table>-->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+<?php include_once("analyticstracking.php") ?>
 </body>
 </html>
