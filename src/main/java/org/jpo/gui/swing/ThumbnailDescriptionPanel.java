13440f5167229054c20bfd45ca6ccd5609b16ab8
Richard Eigenmann <richard.eigenmann@gmail.com>
2020-10-14 23:48:20 +0200

    Remove unused logger

--- src/main/java/org/jpo/gui/swing/ThumbnailDescriptionPanel.java
+++ src/main/java/org/jpo/gui/swing/ThumbnailDescriptionPanel.java
@@ -26,11 +26,8 @@ import javax.swing.*;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import java.awt.*;
-import java.util.logging.Logger;
 
 public class ThumbnailDescriptionPanel extends JPanel {
-
-    private static final Logger LOGGER = Logger.getLogger(ThumbnailDescriptionPanel.class.getName());
 
     /**
      * This object holds the description
