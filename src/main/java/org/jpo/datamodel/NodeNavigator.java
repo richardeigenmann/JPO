f611c35dd6da961d36f22fba487f68d2c098971f
Richard Eigenmann <richard.eigenmann@gmail.com>
2021-02-16 21:40:19 +0100

    Add final keywords

--- src/main/java/org/jpo/datamodel/NodeNavigator.java
+++ src/main/java/org/jpo/datamodel/NodeNavigator.java
@@ -12,8 +12,8 @@ import java.util.Set;
  modify it under the terms of the GNU General Public License
  as published by the Free Software Foundation; either version 2
  of the License, or any later version. This program is distributed
- in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- without even the implied warranty of MERCHANTABILITY or FITNESS
+ in the hope that it will be useful, but WITHOUT ANY WARRANTY.
+ Without even the implied warranty of MERCHANTABILITY or FITNESS
  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
  more details. You should have received a copy of the GNU General Public License
  along with this program; if not, write to the Free Software
@@ -38,16 +38,16 @@ public abstract class NodeNavigator
      * Registers a NodeNavigatorListener
      */
     @Override
-    public void addNodeNavigatorListener( NodeNavigatorListener nodeNavigatorListener ) {
-        nodeNavigatorListeners.add( nodeNavigatorListener );
+    public void addNodeNavigatorListener(final NodeNavigatorListener nodeNavigatorListener) {
+        nodeNavigatorListeners.add(nodeNavigatorListener);
     }
 
     /**
      * Removes a NodeNavigatorListener
      */
     @Override
-    public void removeNodeNavigatorListener( NodeNavigatorListener nodeNavigatorListener ) {
-        nodeNavigatorListeners.remove( nodeNavigatorListener );
+    public void removeNodeNavigatorListener(final NodeNavigatorListener nodeNavigatorListener) {
+        nodeNavigatorListeners.remove(nodeNavigatorListener);
     }
 
     /**
