c0443e841bcbde16580ec17e269ef219b12abaa7
Richard Eigenmann <richard.eigenmann@gmail.com>
2021-12-17 13:40:27 +0100

    Fix homepage for narrow screens

--- web/page-start.php
+++ web/page-start.php
@@ -5,82 +5,83 @@
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <link rel="shortcut icon" href="favicon.ico" />
     </head>
     <body>
     <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
         <div class="container-fluid">
-        <a class="navbar-brand mb-0 h1" href="index.php">JPO Java Picture Organizer</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+            <a class="navbar-brand mb-0 h1" href="index.php">JPO Java Picture Organizer</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
 
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="downloadDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Download
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarSupportedContent">
-                        <li><a class="dropdown-item" href="download.php">Windows 10</a></li>
-                        <li><a class="dropdown-item" href="downloadLinux.php">Linux</a></li>
-                        <li><a class="dropdown-item" href="runJavaDirectly.php">Run Java directly</a></li>
-                    </ul>
-                </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="downloadDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Download
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="downloadDropdown">
+                            <li><a class="dropdown-item" href="download.php">Windows 10</a></li>
+                            <li><a class="dropdown-item" href="downloadLinux.php">Linux</a></li>
+                            <li><a class="dropdown-item" href="runJavaDirectly.php">Run Java directly</a></li>
+                        </ul>
+                    </li>
 
 
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarTutorialsDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Tutorials
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarSupportedContent">
-                        <li><a class="dropdown-item" href="tutorial_gettingstarted.php">Getting Started</a></li>
-                        <li><a class="dropdown-item" href="tutorial_download_from_camera.php">Download from Camera</a></li>
-                        <li><a class="dropdown-item" href="tutorial_order_prints.php">Order prints from a lab</a></li>
-                    </ul>
-                </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarTutorialsDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Tutorials
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarTutorialsDropdown">
+                            <li><a class="dropdown-item" href="tutorial_gettingstarted.php">Getting Started</a></li>
+                            <li><a class="dropdown-item" href="tutorial_download_from_camera.php">Download from Camera</a></li>
+                            <li><a class="dropdown-item" href="tutorial_order_prints.php">Order prints from a lab</a></li>
+                        </ul>
+                    </li>
 
-                <li class="nav-item">
-                    <a class="nav-link" href="support.php">Support</a>
-                </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="support.php">Support</a>
+                    </li>
 
-                <li class="nav-item">
-                    <a class="nav-link" href="license.php">License</a>
-                </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="license.php">License</a>
+                    </li>
 
-                <li class="nav-item">
-                    <a class="nav-link" href="privacy.php">Privacy</a>
-                </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="privacy.php">Privacy</a>
+                    </li>
 
-                <li class="nav-item">
-                    <a class="nav-link" href="about.php">About the Project</a>
-                </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="about.php">About the Project</a>
+                    </li>
 
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Code
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="https://github.com/richardeigenmann/JPO">Github</a></li>
-                        <li><a class="dropdown-item" href="javadoc/index.html">Javadoc</a></li>
-                        <li><hr class="dropdown-divider"></li>
-                        <li><a class="dropdown-item" href="windows.php">Set up Dev environment on Windows 10</a></li>
-                        <li><a class="dropdown-item" href="intellij.php">Set up IntelliJ IDEA Dev environment</a></li>
-                        <li><a class="dropdown-item" href="commandLineDev.php">Command Line Development</a></li>
-                        <li><a class="dropdown-item" href="eclipse.php">Set up Eclipse Dev environment</a></li>
-                        <li><a class="dropdown-item" href="netbeans.php">Set up Netbeans Dev environment</a></li>
-                        <li><hr class="dropdown-divider"></li>
-                        <li><a class="dropdown-item" href="https://sourceforge.net/p/j-po/git/ci/master/tree/">Source Code Sourceforge.net</a></li>
-                        <li><a class="dropdown-item" href="https://sourceforge.net/projects/j-po/">SourceForge project page</a></li>
-                        <li><a class="dropdown-item" href="https://travis-ci.org/github/richardeigenmann/JPO">Travis CI Build status</a></li>
-                        <li><a class="dropdown-item" href="https://sonarcloud.io/dashboard?id=JPO">Sonarcloud Dashboard</a></li>
-                    </ul>
-                </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="codeDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Code
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="codeDropdown">
+                            <li><a class="dropdown-item" href="https://github.com/richardeigenmann/JPO">Github</a></li>
+                            <li><a class="dropdown-item" href="javadoc/index.html">Javadoc</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="windows.php">Set up Dev environment on Windows 10</a></li>
+                            <li><a class="dropdown-item" href="intellij.php">Set up IntelliJ IDEA Dev environment</a></li>
+                            <li><a class="dropdown-item" href="commandLineDev.php">Command Line Development</a></li>
+                            <li><a class="dropdown-item" href="eclipse.php">Set up Eclipse Dev environment</a></li>
+                            <li><a class="dropdown-item" href="netbeans.php">Set up Netbeans Dev environment</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="https://sourceforge.net/p/j-po/git/ci/master/tree/">Source Code Sourceforge.net</a></li>
+                            <li><a class="dropdown-item" href="https://sourceforge.net/projects/j-po/">SourceForge project page</a></li>
+                            <li><a class="dropdown-item" href="https://travis-ci.org/github/richardeigenmann/JPO">Travis CI Build status</a></li>
+                            <li><a class="dropdown-item" href="https://sonarcloud.io/dashboard?id=JPO">Sonarcloud Dashboard</a></li>
+                        </ul>
+                    </li>
 
-            </ul>
-        </div>
+                </ul>
+            </div>
         </div>
     </nav>
+
     <br>
     <br>
