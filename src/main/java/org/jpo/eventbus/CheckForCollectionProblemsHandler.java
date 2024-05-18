0b1afb47133a219c7b6d4347c7b6156ef47949a5
Richard Eigenmann <richard.eigenmann@gmail.com>
2024-05-18 15:07:56 +0200

    CheckForCollectionProblems doesn't rely on a central PictureCollection

--- src/main/java/org/jpo/eventbus/CheckForCollectionProblemsHandler.java
+++ src/main/java/org/jpo/eventbus/CheckForCollectionProblemsHandler.java
@@ -7,7 +7,7 @@ import org.jpo.datamodel.Settings;
 import javax.swing.*;
 
 /*
- Copyright (C) 2022-2023 Richard Eigenmann.
+ Copyright (C) 2022-2024 Richard Eigenmann.
  This program is free software; you can redistribute it and/or
  modify it under the terms of the GNU General Public License
  as published by the Free Software Foundation; either version 2
@@ -35,7 +35,7 @@ public class CheckForCollectionProblemsHandler {
      */
     @Subscribe
     public void handleEvent(final CheckForCollectionProblemsRequest request) {
-        var missingSha256Count = Settings.getPictureCollection().getRootNode()
+        var missingSha256Count = request.pictureCollection().getRootNode()
                 .getChildPictureNodesDFS()
                 .filter(e -> ((PictureInfo) e.getUserObject()).getSha256().equals(""))
                 .count();
