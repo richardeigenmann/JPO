9c4764e4951196b3f60823a721c92431a9d8de19
Richard Eigenmann <richard.eigenmann@gmail.com>
2024-05-18 17:03:18 +0200

    Year is now 2024

--- src/main/java/org/jpo/gui/CollectionJTreeController.java
+++ src/main/java/org/jpo/gui/CollectionJTreeController.java
@@ -22,7 +22,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /*
- * Copyright (C) 2002 - 2023 Richard Eigenmann, Zurich, Switzerland This
+ * Copyright (C) 2002-2024 Richard Eigenmann, Zurich, Switzerland This
  * program is free software; you can redistribute it and/or modify it under the
  * terms of the GNU General Public License as published by the Free Software
  * Foundation; either version 2 of the License, or any later version. This
