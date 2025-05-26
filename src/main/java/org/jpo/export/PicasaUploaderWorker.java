f76d09ebbea43f47b7f50100df2762a383f3b31a
Richard Eigenmann <richard.eigenmann@gmail.com>
2025-05-26 22:08:53 +0200

    Remove dead code

--- src/main/java/org/jpo/export/PicasaUploaderWorker.java
+++ src/main/java/org/jpo/export/PicasaUploaderWorker.java
@@ -5,14 +5,13 @@ import org.jpo.datamodel.PictureInfo;
 import org.jpo.datamodel.SortableDefaultMutableTreeNode;
 
 import javax.swing.*;
-import java.net.URL;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /*
  * PicasaUploaderWorker.java: service using Google provided code. Copyright (C)
- * 2012-2024 Richard Eigenmann. This program is free software; you can
+ * 2012-2025 Richard Eigenmann. This program is free software; you can
  * redistribute it and/or modify it under the terms of the GNU General Public
  * License as published by the Free Software Foundation; either version 2 of the
  * License, or any later version. This program is distributed in the hope that
@@ -86,7 +85,6 @@ public class PicasaUploaderWorker extends SwingWorker<Boolean, Integer> {
     protected void done() {
         doneHandler.uploadDone();
     }
-    private URL albumUrl;
 
     /**
      * Creates a group on Picasa
@@ -94,42 +92,7 @@ public class PicasaUploaderWorker extends SwingWorker<Boolean, Integer> {
      * @return  true if success, false if not
      */
     public boolean createAlbum( GroupInfo groupInfo ) {
-        LOGGER.info( "Creating Album" );
-/*        final AlbumEntry myAlbum = new AlbumEntry();
-        myAlbum.setTitle( new PlainTextConstruct( groupInfo.getGroupName() ) );
-        myAlbum.setDescription( new PlainTextConstruct( groupInfo.getGroupName() ) );
-        AlbumEntry insertedEntry;
-
-        URL feedUrl;
-        try {
-            feedUrl = new URL( myRequest.getFormattedPicasaUrl() );
-        } catch ( MalformedURLException ex ) {
-            LOGGER.severe( ex.getMessage() );
-            return false;
-        }
-
-        try {
-            insertedEntry = myRequest.picasaWebService.insert( feedUrl, myAlbum );
-        } catch ( IOException | ServiceException ex ) {
-            LOGGER.severe( ex.getMessage() );
-            return false;
-        }
-
-        String albumIdUrl = insertedEntry.getId();
-        String albumId = albumIdUrl.substring( albumIdUrl.lastIndexOf( '/' ) + 1 );
-        String albumPostUrl = myRequest.getFormattedPicasaUrl() + "/albumid/" + albumId;
-        myRequest.setAlbumPostUrl(albumPostUrl);
-        LOGGER.log(Level.INFO, "raw: {0} parsed: {1}%nrecombined: {2}", new Object[]{albumIdUrl, albumId, albumPostUrl});
-
-        try {
-            albumUrl = new URL( albumPostUrl );
-        } catch ( MalformedURLException ex ) {
-            LOGGER.severe( ex.getMessage() );
-            return false;
-        }
-        LOGGER.log(Level.INFO, "AlbumId: {0}", albumUrl);
-
- */
+        LOGGER.log(Level.SEVERE, "Not implemented. No longer working" );
         return true;
     }
 
@@ -139,20 +102,6 @@ public class PicasaUploaderWorker extends SwingWorker<Boolean, Integer> {
      * @param pictureInfo the picture to upload
      */
     public void postPicture(final PictureInfo pictureInfo) {
-        LOGGER.log(Level.INFO, "Posting Picture: {0}", pictureInfo.getDescription());
-        /*
-        final PhotoEntry myPhoto = new PhotoEntry();
-        myPhoto.setTitle(new PlainTextConstruct(pictureInfo.getDescription()));
-        myPhoto.setDescription(new PlainTextConstruct(pictureInfo.getDescription()));
-        myPhoto.setClient("JPO");
-
-        final MediaFileSource myMedia = new MediaFileSource(pictureInfo.getImageFile(), "image/jpeg");
-        myPhoto.setMediaSource(myMedia);
-        try {
-            myRequest.picasaWebService.insert(albumUrl, myPhoto);
-        } catch (final IOException | ServiceException ex) {
-            LOGGER.severe(ex.getMessage());
-        }
-         */
+        LOGGER.log(Level.SEVERE, "Not implemented. No longer working" );
     }
 }
