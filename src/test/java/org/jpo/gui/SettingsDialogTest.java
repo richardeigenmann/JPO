dba76234c1517ed07d6eeb67ea71b301c24d1e2c
Richard Eigenmann <richard.eigenmann@gmail.com>
2025-05-28 09:45:27 +0200

    Remove dead code

--- src/test/java/org/jpo/gui/SettingsDialogTest.java
+++ src/test/java/org/jpo/gui/SettingsDialogTest.java
@@ -7,6 +7,22 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+/*
+ Copyright (C) 2021-2025 Richard Eigenmann.
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or any later version. This program is distributed
+ in the hope that it will be useful, but WITHOUT ANY WARRANTY.
+ Without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ more details. You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ The license is in gpl.txt.
+ See http://www.gnu.org/copyleft/gpl.html for the details.
+ */
+
 class SettingsDialogTest {
 
     @BeforeAll
@@ -19,20 +35,9 @@ class SettingsDialogTest {
      */
     @Test
     void testConstructor() {
-        //assumeFalse(GraphicsEnvironment.isHeadless());
         final var jDialog = GuiActionRunner.execute(() -> new SettingsDialog(false));
         assertNotNull(jDialog);
         GuiActionRunner.execute(jDialog::getRid);
-        /*try {
-            SwingUtilities.invokeAndWait(() -> {
-                final SettingsDialog jDialog = new SettingsDialog(false);
-                assertNotNull(jDialog);
-                jDialog.getRid();
-            });
-        } catch (final InterruptedException | InvocationTargetException ex) {
-            fail(ex.getMessage());
-            Thread.currentThread().interrupt();
-        }*/
     }
 
 }
