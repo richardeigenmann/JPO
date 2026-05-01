22f4e338838e6671691247352e1c6b0fac51de2e
Richard Eigenmann <richard.eigenmann@gmail.com>
2026-05-01 12:42:53 +0200

    Package Angular App into Spring Boot Container

--- api/src/main/kotlin/org/jpo/api/ApiApplication.kt
+++ api/src/main/kotlin/org/jpo/api/ApiApplication.kt
@@ -6,12 +6,33 @@ import org.springframework.context.annotation.Bean
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.io.ClassPathResource
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
+import org.springframework.web.servlet.resource.PathResourceResolver
+
 @SpringBootApplication(scanBasePackages = ["org.jpo"])
 class ApiApplication {
 
     @Bean
-    fun corsConfigurer(): WebMvcConfigurer {
+    fun webMvcConfigurer(): WebMvcConfigurer {
         return object : WebMvcConfigurer {
+            override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+                registry.addResourceHandler("/**")
+                    .addResourceLocations("classpath:/static/browser/")
+                    .resourceChain(true)
+                    .addResolver(object : PathResourceResolver() {
+                        override fun getResource(resourcePath: String, location: org.springframework.core.io.Resource): org.springframework.core.io.Resource? {
+                            val requestedResource = location.createRelative(resourcePath)
+                            return if (requestedResource.exists() && requestedResource.isReadable) {
+                                requestedResource
+                            } else {
+                                ClassPathResource("/static/browser/index.html")
+                            }
+                        }
+                    })
+            }
+
             override fun addCorsMappings(registry: CorsRegistry) {
                 registry.addMapping("/**")
                     .allowedOrigins("http://localhost:4200")
